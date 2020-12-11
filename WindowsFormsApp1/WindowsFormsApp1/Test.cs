d5b914cd9ff4d9f9e9d9b1a81afe88ca82fbd3fa
Mark Mansour <mark.mansour@hotmail.se>
2020-12-11 15:07:56 +0100

    Revert "Revert "EF Skeleton""
    This reverts commit 408795a123c8004c44ed9d33a7143ea738a4d1e3.

---
+++ WindowsFormsApp1/WindowsFormsApp1/Test.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace WindowsFormsApp1
+{
+    public class Test
+    {
+        public int Id { get; set; }
+        public List<QuizQuestion> Questions { get; set; }
+        public List<QuizAnswer> Answers { get; set; }
+    }
+}
